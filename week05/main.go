fffabf718693283ff273b97f225010c19249b5ca
sungmin <smyoun115@gmail.com>
2024-09-27 15:15:21 +0900

    v0.2 IO, math, strings

---
+++ week05/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	//var i int = 55
+	//var i int
+	//i = 55
+
+	var f float32 = 3.33
+
+	i := 55
+	fmt.Println(f, math.Floor(3.49))
+	fmt.Println(strings.Title("subin park"))
+	fmt.Printf("i는 %d\n", i)
+	fmt.Print("i는 ", i, "\n")
+	fmt.Println("i는", i)
+}
