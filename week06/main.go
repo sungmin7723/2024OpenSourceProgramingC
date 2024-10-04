74649956483ae462550e16d3bfabfa9c7e4b62ec
sungmin <smyoun115@gmail.com>
2024-10-04 15:05:31 +0900

    v0.6 ASCII

--- week06/main.go
+++ week06/main.go
@@ -5,10 +5,18 @@ import (
 	"reflect"
 )
 
+// ASCII 'A' = 65, 'a' = 97, '0' = 48
 func main() {
-	i := 13              //var i int = 13
-	var f float64 = 12.9 //f := 12.9
-	fmt.Printf("value i: %d, value f: %f\n", i, f)
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
+	// i := 13              //var i int = 13
+	// var f float64 = 12.9 //f := 12.9
+	// fmt.Printf("value i: %d, value f: %f\n", i, f)
+	// fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
+	// fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
+
+	c1 := 'z'
+	c2 := '김'
+
+	fmt.Println(c1, c2)
+	fmt.Println(reflect.TypeOf(c1), reflect.TypeOf(c2))
+
 }
