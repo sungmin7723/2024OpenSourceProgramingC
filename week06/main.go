4bc9c433e43086e6fd64a7e6e307553e4cbbef89
sungmin <smyoun115@gmail.com>
2024-10-04 14:47:29 +0900

    v0.5 convesion

--- week06/main.go
+++ week06/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	var i int = 13
-	f := 12.9
+	i := 13              //var i int = 13
+	var f float64 = 12.9 //f := 12.9
 	fmt.Printf("value i: %d, value f: %f\n", i, f)
-	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
-	fmt.Println(reflect.TypeOf(i))
+	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 }
