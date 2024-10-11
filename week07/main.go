b9259f9dad3d1a67bae2b39c2c5da4bdcb4c4895
sungmin <smyoun115@gmail.com>
2024-10-11 15:27:51 +0900

    v1.3 Parse

--- week07/main.go
+++ week07/main.go
@@ -5,28 +5,27 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	// //오늘은 2024년 10월 11일 입니다.
-	// var now time.Time = time.Now()
-	// //var month int = int(now.Month()) //var month time.Month = now.Month()
-	// //fmt.Println(month)
-	// fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", now.Year(), int(now.Month()), now.Day())
-	// fmt.Printf("지금은 %d시 %d분 %d초 입니다.", now.Hour(), now.Minute(), now.Second())
-
-	// var army string = "우리는 $가와 $민에 충성을 다하는 대한민$ 육군이다"
-	// armyfixed := strings.NewReplacer("$", "국")
-	// fmt.Println(armyfixed.Replace(army))
-
 	in := bufio.NewReader(os.Stdin)
-	fmt.Print("input your name: ")
-	name, err := in.ReadString('\n')
+	fmt.Print("Input your score : ")
+	i, err := in.ReadString('\n')
 
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		fmt.Println(name)
 	}
-
+	i = strings.TrimSpace(i)                // 줄바꿈등 제거. 파이썬의 strip 함수와 비슷
+	score, _ := strconv.ParseInt(i, 10, 32) // 10진 정수형(32bit)으로 변환
+	if score >= 60 {
+		//status := "passing"
+		fmt.Println("A")
+		fmt.Printf("%d", score)
+	} else {
+		//status := "failing"
+		fmt.Println("BCDF")
+		fmt.Printf("%d", score)
+	}
 }
