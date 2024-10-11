5f3610ed87205699e781363aa03df40297c40e4c
sungmin <smyoun115@gmail.com>
2024-10-11 14:33:21 +0900

    v1.2 If

--- week07/main.go
+++ week07/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -21,7 +22,11 @@ func main() {
 	in := bufio.NewReader(os.Stdin)
 	fmt.Print("input your name: ")
 	name, err := in.ReadString('\n')
-	fmt.Println(name)
-	fmt.Println(err)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
 
 }
