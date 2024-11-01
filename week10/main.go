6860c494ed7c0bde96ac767458382f03122ead08
hyeon <smyoun115@gmail.com>
2024-11-01 15:17:25 +0900

    v2.6

--- week10/main.go
+++ week10/main.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	fmt.Printf("%f \n", math.Sqrt(16.0))
 	in := bufio.NewReader(os.Stdin)
 	fmt.Print("Input Number : ")
 	i, err := in.ReadString('\n')
@@ -29,7 +31,7 @@ func main() {
 		isPrime = false
 	} else {
 		j := 2
-		for j < n {
+		for j <= int(math.Sqrt(float64(n))) {
 			if n%j == 0 {
 				// count++
 				isPrime = false // 더하기 연산 제거
