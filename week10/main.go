0af0b41cb7e2bc44b8d588fa394a3af98ddcc523
hyeon <smyoun115@gmail.com>
2024-11-01 14:07:57 +0900

    v2.1 prime number

---
+++ week10/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	fmt.Print("Input Number : ")
+	i, err := in.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i) // 줄바꿈등 제거. 파이썬의 strip 함수와 비슷
+	n, _ := strconv.Atoi(i)  // 10진 정수형(32bit)으로 변환
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	count := 0
+
+	j := 1
+	for j <= n {
+		if n%j == 0 {
+			count++
+		}
+		j++
+	}
+	if count == 2 {
+		fmt.Printf("%d is prime number", n)
+	} else {
+		fmt.Printf("%d is Not prime number", n)
+
+	}
+}
